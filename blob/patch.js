dcf7d00d6d34ebdf899555d4bee416d3f4a1fdcd
Guy L <g.guy.lab@gmail.com>
2021-10-31 18:19:39 -0400

    renamed patch + added necessary config files

---
+++ blob/patch.js
@@ -0,0 +1,7 @@
+{
+    "id": "blob",
+    "servers": [
+        "https://mirrors.thpatch.net/Guy/blob/"
+    ],
+    "title": "Replaces BGM by Thomas Light's wonderful Blob Opera renditions (+minor graphical changes)."
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "Guy#8081",
+    "id": "Guy",
+    "patches": {
+        "blob": "Replaces BGM by Thomas Light's wonderful Blob Opera renditions (+minor graphical changes)."
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/Guy/"
+    ],
+    "title": "Guy's patches"
+}
