3afcbd17c236ff7de33f094f241d816edaafe24f
Guy L <g.guy.lab@gmail.com>
2022-04-27 15:58:20 -0400

    will smith bombwhack slap patch

--- repo.js
+++ repo.js
@@ -4,7 +4,8 @@
     "patches": {
         "blob": "Replaces BGM by Thomas Light's wonderful Blob Opera renditions (+minor graphical changes).",
 		"greenscreen": "Replaces graphical elements to easily key them out for transparent screenshots of patterns. Invincibility is recommended.",
-		"sightseeing": "Removes stage logic and player sprites to take pictures of the backgrounds."
+		"sightseeing": "Removes stage logic and player sprites to take pictures of the backgrounds.",
+		"slap": "Keeps Remi's name out their fucking mouthes."
     },
     "servers": [
         "https://mirrors.thpatch.net/Guy/"
---
+++ slap/patch.js
@@ -0,0 +1,7 @@
+{
+    "id": "slap",
+    "servers": [
+        "https://mirrors.thpatch.net/Guy/slap/"
+    ],
+    "title": "Keeps Remi's name out their fucking mouthes"
+}
